a39549ec79486ca34ddf81f6e409bbc70c336374
adwd <ecdt.mn@gmail.com>
2018-03-21 01:58:55 +0900

    add html file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { sub } from './sub';
 
 console.log(sub(10, 3));
+
+const root = document.getElementById('root') as HTMLDivElement;
+root.textContent = 'hei';
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,15 @@
 import webpack from 'webpack';
+import path from 'path';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config: webpack.Configuration = {
   mode: 'production',
   entry: './src/index.ts',
+  output: {
+    filename: '[chunkhash].bundle.js'
+  },
   resolve: {
     extensions: ['.ts', '.tsx']
   },
@@ -10,7 +17,17 @@ const config: webpack.Configuration = {
     rules: [
       { test: /\.tsx?$/, use: 'ts-loader' }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+    }),
+    new CopyWebpackPlugin([{
+      from: './public',
+      to: ''
+    }]),
+  ]
 }
 
 export default config;
